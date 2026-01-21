630cd4b1766cb67d90f562c7d86765711617e1b8
Anthony <asimone2@ramapo.edu>
2026-01-21 15:57:47 -0500

    Continued experimentation on file layouts

---
+++ projects/project1/dev/src/sourceFiles/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main ( int argc, char *argv[] )
+{
+
+    std::cout << "Program started\n";
+
+    // Create the tournament
+    // Create the round
+    // Create players
+
+
+    return 0;
+}
